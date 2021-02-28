1f2b6ae64f07ee0499c75afff7165ed48275fa3c
Murilo <murilopaulino@live.com>
2021-02-28 18:03:02 -0300

    criação da classe ExercicioDoWhile

---
+++ Exercicios/src/exercicios2502/ExercicioDoWhileDois.java
@@ -0,0 +1,25 @@
+package exercicios2502;
+import java.util.Scanner;
+
+public class ExercicioDoWhileDois {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		int numero, soma = 0, contador = 0;
+		double media = 0;
+
+		do {
+			System.out.print("Digite um número: ");
+			numero = leia.nextInt();
+			if (numero != 0 && numero % 3 == 0) {
+				contador++;
+				soma += numero;
+				media = soma / contador;
+			}
+		} while (numero != 0);
+		System.out.println("A média do nº múltiplo de 3 é: " + media);
+	}
+}
+//Escrever um programa que receba vários números inteiros no teclado. 
+//E no final imprimir a média dos números múltiplos de 3. Para sair digitar 0
