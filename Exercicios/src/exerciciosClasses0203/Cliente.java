b2c44de19bcbd55c5e4870cf6d1eb596b12cc690
Murilo <murilopaulino@live.com>
2021-03-02 14:46:19 -0300

    criação da classe Cliente

---
+++ Exercicios/src/exerciciosClasses0203/Cliente.java
@@ -0,0 +1,44 @@
+package exerciciosClasses0203;
+
+public class Cliente {
+	
+	private String nome;
+	private String email;
+	private String cpf;
+	private int anoNascimento;
+	private String telefone;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getAnoNascimento() {
+		return anoNascimento;
+	}
+	public void setAnoNascimento(int anoNascimento) {
+		this.anoNascimento = anoNascimento;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
